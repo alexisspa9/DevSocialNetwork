432d7ca5d3649e5a22df98e863e19e408dea9c90
alexisspa9 <alexisspa9@gmail.com>
2019-11-14 18:50:49 +0200

    basic route structure

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ const app = express();
 
 connectDB();
 
-app.get('/', (req, res) => res.send('Api is running'))
+app.get('/', (req, res) => res.send('Api is running'));
 
-const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`)})
+// Define Routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`)});
