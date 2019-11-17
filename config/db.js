79fa0def169df6dbf8fbbdeef5c5aecad414f52a
alexisspa9 <alexisspa9@gmail.com>
2019-11-17 16:17:54 +0200

    Create Profile and Update profile routes

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const connectDB = async () => {
             { 
                 useNewUrlParser: true,
                 useCreateIndex: true,
-                useUnifiedTopology: true
+                useUnifiedTopology: true,
+                useFindAndModify: false
             });
 
         console.log('MongoDB connected...')
