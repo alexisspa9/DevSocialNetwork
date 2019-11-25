af4a0f324a10842fbbb019ede56abe93fce570f9
alexisspa9 <alexisspa9@gmail.com>
2019-11-25 22:32:47 +0200

    added redux and user authentication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,12 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 
-const Register = () => {
+import PropTypes from 'prop-types';
+
+const Register = ({ setAlert, register , isAuthenticated}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -14,12 +19,19 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
-            console.log('Passwords do not match');
+            setAlert('Passwords do not match', 'danger');
         } else {
-            console.log('SUCCESS');
+            register({ name, email, password });
             
         }
     }
+
+
+    if (isAuthenticated) {
+        return <Redirect to='/dashboard' />
+    }
+
+
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
@@ -60,4 +72,15 @@ Gravatar email</small>
     );
 }
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+}
+
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
